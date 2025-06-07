8e02f7cf0d9fb22c7670dc80ffa014a10c927398
nargiiziii <ibrahimlinargiz169@gmail.com>
2025-06-07 10:11:56 -0800

    everything are working

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Logo from "../logo/Logo";
 import Navlist from "../navlist/Navlist";
 import style from "./Navbar.module.scss";

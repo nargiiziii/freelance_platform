886f4924a59e2d6ac8bc03e14a9b0357ad91a92f
nargiiziii <ibrahimlinargiz169@gmail.com>
2025-06-08 02:42:50 -0800

    some checks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
+  base: './', // или '/название-папки/' если деплоишь не в корень
   plugins: [react()],
-})
+});
+
+
